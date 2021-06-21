dfb2a45c367873c818b610fabec446f5c3821070
AdvaitDhingra <advait.dhingra1006@gmail.com>
2021-06-21 20:45:17 +0200

    Cool new features

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -6,10 +6,12 @@ import Header from "../components/header";
 
 export default function Profile() {
   const [profilePicture, setProfilePicture] = useState("");
+  const [emailAddress, setEmailAddress] = useState("");
 
-  const updatePic = () => {
+  const update = () => {
     fire.auth().currentUser.updateProfile({
       photoURL: profilePicture,
+      email: emailAddress
     });
   };
 
@@ -21,18 +23,26 @@ export default function Profile() {
           padding: "20px 20px",
           display: "flex",
           flexDirection: "column",
+          maxWidth: "500px"
         }}
       >
         <Typography variant="h5">Profil Bearbeiten</Typography>
         <br></br>
+        <Typography variant = "h6">Profilbild:</Typography>
         <Input
           defaultValue={fire.auth().currentUser.photoURL}
           type="text"
           placeholder="Profilbild URL"
           onChange={(e) => setProfilePicture(e.target.value)}
         />
+        <br></br>
+        <Input 
+          type = "email"
+          defaultValue = {fire.auth().currentUser.email}
+          placeholder = "Email Adresse"
+          onChange={(e) => setEmailAddress(e.target.value) }/>
         <Button
-          onClick={updatePic}
+          onClick={update}
           style={{
             width: "100px",
           }}
