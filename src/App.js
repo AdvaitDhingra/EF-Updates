7ea60cdab9fce71db896a8a924a062e3ccfd7835
Arthur Pacaud <arthur.pacaud@hotmail.fr>
2021-06-22 21:33:36 +0200

    Remove unused files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { CssBaseline } from "@material-ui/core";
 
 import Profile from "./pages/Profile";
 import LandingPage from "./pages/LandingPage";
@@ -8,6 +9,7 @@ import { Switch, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
+      <CssBaseline />
       <Switch>
         <Route exact path="/" component={LandingPage} />
         <Route path="/profil" component={Profile} />
